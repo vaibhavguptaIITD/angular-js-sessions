c51169681c6d427d03e1d8f873ceb08735e4e03e
Vaibhav Gupta <vaibhavgupta@Vaibhav-Gupta.local>
2014-11-24 00:27:39 +0530

    Adding static html

---
+++ scripts/data.js
@@ -0,0 +1,16 @@
+var data = 
+	[{
+		"id": 1,
+		"name" : "Item1",
+		"done" : false
+	},
+	{
+		"id": 2,
+		"name" : "Item2",
+		"done" : false	
+	},
+	{
+		"id": 3,
+		"name" : "Item3",
+		"done" : false	
+	}]
