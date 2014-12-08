88f991ca66a63bed1a1181617c86cac493438606
Vaibhav Gupta <vaibhavgupta@Vaibhav-Gupta.local>
2014-12-08 11:54:22 +0530

    Adding files for directives template

--- directives/script.js
+++ directives/script.js
@@ -1,57 +1,9 @@
 angular.module("directiveDemo",[])
-.directive("sibling1", function(){
-	return {
-		controller: function($scope){
-			this.methodA = function(){
-				console.log("in method A of sibling1");
-			}
-		}
-	}
+.controller("DemoController", function($scope){
+	$scope.fruits = ["Apple","Oranges","Banana","Peach"];
 })
-.directive("sibling2", function(){
+.directive("listFruits", function(){
 	return {
-		require : "sibling1",
-		link : function(scope, element, attrs, sibling1Controller){
-			sibling1Controller.methodA();
-		}
+		templateUrl : "/listFruits.html"
 	}
-})
-.directive("parent", function(){
-	return {
-		controller : function(){
-			this.methodA = function(){
-				console.log("in methodA of parent");
-			};
-		}
-	}
-})
-.directive("child", function(){
-	return {
-		require : "^parent",
-		link: function(scope, element, attrs, parentController){
-			parentController.methodA();
-		}
-	}
-})
-.directive("optionalDirective", function(){
-	return {
-		controller : function(){
-			this.methodA = function(){
-				console.log("in methodA of optionalDirective");
-			};
-		}
-	}
-})
-.directive("directive1", function(){
-	return {
-		require: "?optionalDirective",
-		link: function(scope, element, attrs, optionalDirectiveController){
-			if(optionalDirectiveController){
-				optionalDirectiveController.methodA();
-			}
-		}
-	}
-})
-.controller("DemoController", function(){
-
 });
