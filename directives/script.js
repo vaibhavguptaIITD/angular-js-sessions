62b3e2bef5ed7527dbca8a4fb2707ac6f38d0eea
Vaibhav Gupta <vaibhavgupta@Vaibhav-Gupta.local>
2014-12-08 15:53:14 +0530

    Adding files for directivesNgModel

--- directives/script.js
+++ directives/script.js
@@ -1,9 +1,53 @@
 angular.module("directiveDemo",[])
 .controller("DemoController", function($scope){
-	$scope.fruits = ["Apple","Oranges","Banana","Peach"];
+	$scope.employee = {
+		name: "Vaibhav Gupta"
+	};
+	$scope.changeEmployeeName = function(){
+		$scope.employee.name = "Vaibhav Gupta " + _.random(0, 100);
+	};
+	$scope.addHyphen = function(){
+		$scope.employee.name += "- -";
+	};
 })
-.directive("listFruits", function(){
+.directive("contentarea", function($timeout){
 	return {
-		templateUrl : "/listFruits.html"
+		require: "ngModel",
+		priority: 1,
+		link : function(scope, element, attrs, ngModelCtrl){
+			//Time out is required because prior to digest cycle the following code shows $viewValue, $modelValue as NaN
+			$timeout(function(){
+				//console.log("viewValue", ngModelCtrl.$viewValue);
+				//console.log("modelValue", ngModelCtrl.$modelValue);
+			});
+			//This will be called when model changes
+			ngModelCtrl.$render = function(){
+				element.html(ngModelCtrl.$viewValue);
+			}
+
+			 element.on('blur keyup change', function() {
+        		ngModelCtrl.$setViewValue(element.html());
+      		 });
+		}
+	}
+})
+.directive("unhyphenate", function(){
+	return {
+		require: "ngModel",
+		link : function(scope, element, attrs, ngModelCtrl){
+			function removeHyphen(value){
+				return value.replace(/\-/g, "");
+			};
+			ngModelCtrl.$parsers.push(function(value){
+				value = removeHyphen(value);
+				console.log("value", value);
+				return value;
+			});
+			ngModelCtrl.$formatters.push(function(value){
+				value = removeHyphen(value);
+				console.log("formatter value", value);
+				return value;
+			});
+		}
 	}
 });
