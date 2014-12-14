ec08fb202754ff33378bbdd9419e6dbff17a606d
Vaibhav Gupta <vaibhavgupta@Vaibhav-Gupta.local>
2014-12-14 20:44:25 +0530

    Adding providerDemo files

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
 angular.module("TodoApp",[])
+.config(function(TodoServiceProvider){
+	TodoServiceProvider.setUrl("http://localhost:2403/items");
+})
 .controller("TodoController", function($scope, TodoService){
 
 	TodoService.getItems().then(function(items){
@@ -21,13 +24,14 @@ angular.module("TodoApp",[])
 	}
 	
 })
-.factory("TodoService", function($http){
+.provider("TodoService", function(){
 
-	var url = "http://localhost:2403/items";
+	var url, http;
+	
 	var items = null;
 
 	var getItems = function(){
-		return $http.get(url).then(function(resp){
+		return http.get(url).then(function(resp){
 			items = resp.data;
 			return items;
 		});
@@ -38,27 +42,36 @@ angular.module("TodoApp",[])
 			name: itemName,
 			done : false
 		}
-		return $http.post(url, item).then(function(response){
+		return http.post(url, item).then(function(response){
 			items.push(response.data);
 		});
 	}
 
 	var editItem = function(item){
 		var editUrl = url + "/" + item.id;
-		return $http.put(editUrl, item);
+		return http.put(editUrl, item);
 	}
 
 	var removeItem = function(item){
 		var deleteUrl = url + "/" + item.id;
-		return $http.delete(deleteUrl).then(function(){
+		return http.delete(deleteUrl).then(function(){
 			items.splice(items.indexOf(item), 1);
 		});
 	}
 
-	return {
-		getItems : getItems,
-		editItem : editItem,
-		removeItem : removeItem,
-		addItem : addItem
-	};
+	this.setUrl = function(Url){
+		url = Url;
+	}
+
+	this.$get = function($http){
+		http = $http;
+		return {
+			getItems : getItems,
+			editItem : editItem,
+			removeItem : removeItem,
+			addItem : addItem
+		};	
+	}
+
+	
 });
