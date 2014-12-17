0ef2f7f10862fa3c42142935df66ac09fe115f3a
Vaibhav Gupta <vaibhavgupta@Vaibhav-Gupta.local>
2014-12-17 16:42:47 +0530

    Adding files for customFiltersDemo

--- app/app.js
+++ app/app.js
@@ -1,4 +1,21 @@
 angular.module("FilterApp",[])
 .controller("FilterController", function($scope){
 	$scope.employee = {};
+})
+.filter('reverse', function() {
+  return function(input, uppercase) {
+  	return reverseString(input, uppercase);
+  };
 });
+
+function reverseString(input, uppercase){
+	input = input || '';
+    var out = "";
+    for (var i = 0; i < input.length; i++) {
+      out = input.charAt(i) + out;
+    }
+    if (uppercase) {
+      out = out.toUpperCase();
+    }
+    return out;
+}
